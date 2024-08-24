a1f875d9e722c7b8505c80e3d3ea331c9c99b3c9
MTokur <mehmettokur@hotmail.com>
2024-08-24 13:30:26 +0300

    merge emir

--- src/git_github/Çalisma.java
+++ src/git_github/Çalisma.java
@@ -7,6 +7,7 @@ public class Çalisma {
 
         System.out.println( "Hello world!"   );
         System.out.println("deneme çalışması");
+        System.out.println(" driff komutu deneme");
 
 
     }
