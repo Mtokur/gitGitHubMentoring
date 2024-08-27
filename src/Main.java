d9a29dd28913a1f29741c39d2fcb1a9c088b8e1a
vazgecen <svazgecen@hotmail.com>
2024-08-27 17:54:59 +0300

    Tokur işin bitecek

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("bu conflict çalışması");
         System.out.println("bu conflict çalışması");
-        System.out.println("bu conflict çalışması");
-        System.out.println("bu conflict çalışması");
+
+
+
+
+        System.out.println("Değişlik gerçekleşiyor");
+        System.out.println("Değişlik gerçekleşiyor");
+        System.out.println("Değişlik gerçekleşiyor Tokuuur");
+
+
+
+
 
 
     }
