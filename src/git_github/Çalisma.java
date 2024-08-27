3867ba879410353702a2be0a85061f513a7dccb4
ayse1 <ayse10121984@gmail.com>
2024-08-27 18:36:53 +0300

    değişiklik3

--- src/git_github/Çalisma.java
+++ src/git_github/Çalisma.java
@@ -4,6 +4,8 @@ public class Çalisma {
 
 
     public static void main(String[] args) {
+        //sayfayı yeniden düzenliyorum :) arada iki satır boşluk olmalı
+
 
         System.out.println( "Hello world!"   );
         System.out.println("deneme çalışması");
