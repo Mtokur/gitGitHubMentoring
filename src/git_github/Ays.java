c839c8f63993170637b9249d75b09ae82fcf8497
ayse1 <ayse10121984@gmail.com>
2024-08-27 18:40:45 +0300

    değişiklik3

--- src/git_github/Ays.java
+++ src/git_github/Ays.java
@@ -2,6 +2,8 @@ package git_github;
 
 public class Ays {
     public static void main(String[] args) {
+        //Push etmeden yapılan değişiklil main de görünüyormu kontrol ediliyor: değişiklik yapıldı, class mavi renk aldı
+        // ve commit yapılmadan main kontrol edildi ve diğişiklik mainde görüldü
         System.out.println("Merhaba");
         System.out.println("Merhaba");
         System.out.println("Merhaba");
