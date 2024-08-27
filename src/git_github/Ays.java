097cc4e54aa8ca7afff3d6132e95df63d5e2b404
ayse1 <ayse10121984@gmail.com>
2024-08-27 18:19:32 +0300

    değişiklik1

---
+++ src/git_github/Ays.java
@@ -0,0 +1,10 @@
+package git_github;
+
+public class Ays {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+    }
+}
